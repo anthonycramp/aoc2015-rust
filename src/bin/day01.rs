57bc4a58b6168a0fcbe145588c98b5f6f0d68e46
Anthony Cramp <anthony.cramp@gmail.com>
2021-06-04 21:02:36 +0930

    Completed solutino for Day 01 Part 1
    Interesting Rust things (re)discovered:
    * include_str! for bringing the string content into the static scope
    * #[cfg(test)] to tell cargo test where to look for tests
    * cargo test --bins
    * cargo run --bin <binary name>
    * .chars() iterate over characters in a string
    * .count() count the number of items in an iterator

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,7 +1,14 @@
-fn main() {}
+const INPUT: &str = include_str!("../../problem_inputs/day01.txt");
+
+fn main() {
+    println!("Day 01: {}", follow_directions(INPUT));
+}
 
 fn follow_directions(directions: &str) -> i32 {
-    0
+    let floors_up = directions.chars().filter(|&c| c == '(').count() as i32;
+    let floors_down = directions.chars().filter(|&c| c == ')').count() as i32;
+
+    floors_up - floors_down
 }
 
 #[cfg(test)]
@@ -9,7 +16,62 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_null() {
+    fn test_empty() {
         assert_eq!(follow_directions(""), 0);
+    }
+
+    #[test]
+    fn day01_problem_tests() {
+        struct Test {
+            directions: &'static str,
+            expected_floor: i32,
+        }
+
+        let test_cases = [
+            Test {
+                directions: "(())",
+                expected_floor: 0,
+            },
+            Test {
+                directions: "()()",
+                expected_floor: 0,
+            },
+            Test {
+                directions: "(((",
+                expected_floor: 3,
+            },
+            Test {
+                directions: "(()(()(",
+                expected_floor: 3,
+            },
+            Test {
+                directions: "))(((((",
+                expected_floor: 3,
+            },
+            Test {
+                directions: "())",
+                expected_floor: -1,
+            },
+            Test {
+                directions: "))(",
+                expected_floor: -1,
+            },
+            Test {
+                directions: ")))",
+                expected_floor: -3,
+            },
+            Test {
+                directions: ")())())",
+                expected_floor: -3,
+            },
+        ];
+
+        for Test {
+            directions,
+            expected_floor,
+        } in test_cases.iter()
+        {
+            assert_eq!(follow_directions(directions), *expected_floor);
+        }
     }
 }
