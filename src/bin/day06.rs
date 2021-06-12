1da6c9837d69e5c6adc2bd8ce847f0b3528b4b26
Anthony Cramp <anthony.cramp@gmail.com>
2021-06-12 23:43:23 +0930

    Added test for parsing input

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -51,3 +51,52 @@ impl From<&str> for Action {
 fn main() {
     Action::from("turn on 0,0 through 999,999");
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_parse_on() {
+        let input = "turn on 0,0 through 999,999";
+        let action = Action::from(input);
+        match action {
+            Action::On(rect) => {
+                assert_eq!(rect.lower_left.x, 0);
+                assert_eq!(rect.lower_left.y, 0);
+                assert_eq!(rect.upper_right.x, 999);
+                assert_eq!(rect.upper_right.y, 999);
+            }
+            _ => panic!("Did not decode an On action"),
+        }
+    }
+
+    #[test]
+    fn test_parse_off() {
+        let input = "turn off 499,499 through 500,500";
+        let action = Action::from(input);
+        match action {
+            Action::Off(rect) => {
+                assert_eq!(rect.lower_left.x, 499);
+                assert_eq!(rect.lower_left.y, 499);
+                assert_eq!(rect.upper_right.x, 500);
+                assert_eq!(rect.upper_right.y, 500);
+            }
+            _ => panic!("Did not decode an Off action"),
+        }
+    }
+
+    #[test]
+    fn test_parse_toggle() {
+        let input = "toggle 0,0 through 999,0";
+        let action = Action::from(input);
+        match action {
+            Action::Toggle(rect) => {
+                assert_eq!(rect.lower_left.x, 0);
+                assert_eq!(rect.lower_left.y, 0);
+                assert_eq!(rect.upper_right.x, 999);
+                assert_eq!(rect.upper_right.y, 0);
+            }
+            _ => panic!("Did not decode a Toggle action"),
+        }
+    }
+}
