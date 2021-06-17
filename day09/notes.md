8c0e67a5c86a58f66de5f185abca2e8e58baee7a
Anthony Cramp <anthony.cramp@gmail.com>
2021-06-17 22:04:06 +0930

    Day 09 Part 1 solution

--- day09/notes.md
+++ day09/notes.md
@@ -1,3 +1,14 @@
 # Advent of Code 2015 Day 09
 
 [Advent of Code 2015 Day 09](https://adventofcode.com/2015/day/9)
+
+## Thoughts
+
+- Use regex to parse the input
+- Compile a set of unique place names
+- Maintain input data as a HashMap with keys (String, String) and values u32
+- Each entry in the input, X to Y = z, results in two entries (x,y) -> z and
+  (y,x) -> z
+- Generate all permutations of the place names
+- Walk each permutation two at a time, looking up distances, and aumming the
+  result
--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,3 +1,10 @@
+use itertools::Itertools;
+use regex::Regex;
+use std::collections::{HashMap, HashSet};
+
+#[macro_use]
+extern crate lazy_static;
+
 const INPUT: &str = include_str!("day09.txt");
 
 fn main() {
@@ -7,12 +14,56 @@ fn main() {
 
 // replace return type as required by the problem
 fn part1(input: &str) -> i32 {
-    0
+    let (routes, places) = parse_routes(input);
+    places
+        .iter()
+        .cloned()
+        .permutations(places.len())
+        .map(|place_seq| compute_distance(&place_seq, &routes))
+        .min()
+        .unwrap()
 }
 
 // replace return type as required by the problem
 fn part2(input: &str) -> i32 {
     0
+}
+
+fn parse_route(input: &str) -> ((String, String), i32) {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"(.*) to (.*) = (.*)").unwrap();
+    }
+
+    let fields = RE.captures(input).unwrap();
+    (
+        (
+            String::from(fields.get(1).unwrap().as_str()),
+            String::from(fields.get(2).unwrap().as_str()),
+        ),
+        fields.get(3).unwrap().as_str().parse().unwrap(),
+    )
+}
+
+fn parse_routes(input: &str) -> (HashMap<(String, String), i32>, HashSet<String>) {
+    let mut routes = HashMap::new();
+    let mut places = HashSet::new();
+
+    for line in input.lines() {
+        let ((place_a, place_b), dist) = parse_route(line);
+        routes.insert((place_a.clone(), place_b.clone()), dist);
+        routes.insert((place_b.clone(), place_a.clone()), dist);
+        places.insert(place_a);
+        places.insert(place_b);
+    }
+
+    (routes, places)
+}
+
+fn compute_distance(place_seq: &[String], routes: &HashMap<(String, String), i32>) -> i32 {
+    place_seq
+        .windows(2)
+        .map(|pair| routes.get(&(pair[0].clone(), pair[1].clone())).unwrap())
+        .sum()
 }
 
 #[cfg(test)]
@@ -22,35 +73,19 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        let test_cases = [
-            TestCase {
-                input: "...",
-                expected: 123,
-            },
-            TestCase {
-                input: "abc",
-                expected: 345,
-            },
-        ];
-        for TestCase { input, expected } in test_cases.iter() {
-            assert_eq!(part1(*input), *expected);
-        }
+        let input = r"London to Dublin = 464
+London to Belfast = 518
+Dublin to Belfast = 141";
+
+        assert_eq!(part1(input), 605);
     }
 
     #[test]
     fn test_part2() {
-        let test_cases = [
-            TestCase {
-                input: "...",
-                expected: 123,
-            },
-            TestCase {
-                input: "abc",
-                expected: 345,
-            },
-        ];
-        for TestCase { input, expected } in test_cases.iter() {
-            assert_eq!(part2(*input), *expected);
-        }
+        let input = r"London to Dublin = 464
+London to Belfast = 518
+Dublin to Belfast = 141";
+
+        assert_eq!(part2(input), 605);
     }
 }
