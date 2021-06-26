8d3a0e0f958e5943017217d2da6943e1998df2a3
Anthony Cramp <anthony.cramp@gmail.com>
2021-06-26 23:14:45 +0930

    Day 12 Part 1 solution

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -5,9 +5,16 @@ fn main() {
     println!("Day 12 Part 2: {:?}", part2(INPUT));
 }
 
+fn is_json_punctuation(c: char) -> bool {
+    r#"[]{},":"#.contains(c)
+}
+
 // replace return type as required by the problem
 fn part1(input: &str) -> i32 {
-    0
+    input
+        .split(is_json_punctuation)
+        .map(|s| if let Ok(v) = s.parse::<i32>() { v } else { 0 })
+        .sum()
 }
 
 // replace return type as required by the problem
@@ -24,12 +31,36 @@ mod tests {
     fn test_part1() {
         let test_cases = [
             TestCase {
-                input: "...",
-                expected: 123,
+                input: "[1,2,3]",
+                expected: 6,
             },
             TestCase {
-                input: "abc",
-                expected: 345,
+                input: r#"{"a":2,"b":4}"#,
+                expected: 6,
+            },
+            TestCase {
+                input: r#"[[[3]]]"#,
+                expected: 3,
+            },
+            TestCase {
+                input: r#"{"a":{"b":4},"c":-1}"#,
+                expected: 3,
+            },
+            TestCase {
+                input: r#"{"a":[-1,1]}"#,
+                expected: 0,
+            },
+            TestCase {
+                input: r#"[-1,{"a":1}]"#,
+                expected: 0,
+            },
+            TestCase {
+                input: r#"[]"#,
+                expected: 0,
+            },
+            TestCase {
+                input: r#"{}"#,
+                expected: 0,
             },
         ];
         for TestCase { input, expected } in test_cases.iter() {
@@ -37,20 +68,20 @@ mod tests {
         }
     }
 
-    #[test]
-    fn test_part2() {
-        let test_cases = [
-            TestCase {
-                input: "...",
-                expected: 123,
-            },
-            TestCase {
-                input: "abc",
-                expected: 345,
-            },
-        ];
-        for TestCase { input, expected } in test_cases.iter() {
-            assert_eq!(part2(*input), *expected);
-        }
-    }
+    // #[test]
+    // fn test_part2() {
+    //     let test_cases = [
+    //         TestCase {
+    //             input: "...",
+    //             expected: 123,
+    //         },
+    //         TestCase {
+    //             input: "abc",
+    //             expected: 345,
+    //         },
+    //     ];
+    //     for TestCase { input, expected } in test_cases.iter() {
+    //         assert_eq!(part2(*input), *expected);
+    //     }
+    // }
 }
