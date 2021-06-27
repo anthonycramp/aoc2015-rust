082c93c2edf1c2ab4cf7ec86d38e14385438bb83
Anthony Cramp <anthony.cramp@gmail.com>
2021-06-27 21:27:07 +0930

    Added code to parse input lines

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,3 +1,8 @@
+use regex::Regex;
+
+#[macro_use]
+extern crate lazy_static;
+
 const INPUT: &str = include_str!("day13.txt");
 
 fn main() {
@@ -15,11 +20,51 @@ fn part2(input: &str) -> i32 {
     0
 }
 
+fn parse(input: &str) -> ((String, String), i32) {
+    lazy_static! {
+        static ref RE: Regex =
+            Regex::new(r"(.*) would (.*) (.*) happiness units by sitting next to (.*).").unwrap();
+    }
+
+    let fields = RE.captures(input).unwrap();
+    (
+        (
+            String::from(fields.get(1).unwrap().as_str()),
+            String::from(fields.get(4).unwrap().as_str()),
+        ),
+        if let Ok(v) = fields.get(3).unwrap().as_str().parse() {
+            if fields.get(2).unwrap().as_str() == "gain" {
+                v
+            } else {
+                -v
+            }
+        } else {
+            panic!("Couldn't parse happiness: {:?}", fields.get(3));
+        },
+    )
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use test_support::test_support::TestCase;
 
+    #[test]
+    fn test_parse() {
+        let input = "Alice would gain 54 happiness units by sitting next to Bob.";
+        let parsed = parse(input);
+
+        assert_eq!(parsed.0 .0, "Alice");
+        assert_eq!(parsed.0 .1, "Bob");
+        assert_eq!(parsed.1, 54);
+
+        let input = "Carol would lose 62 happiness units by sitting next to Alice.";
+        let parsed = parse(input);
+
+        assert_eq!(parsed.0 .0, "Carol");
+        assert_eq!(parsed.0 .1, "Alice");
+        assert_eq!(parsed.1, -62);
+    }
     #[test]
     fn test_part1() {
         let test_cases = [
