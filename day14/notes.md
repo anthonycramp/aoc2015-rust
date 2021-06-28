b63393dd3e9f3601b35e342ad4ffd2c3f7f5eaef
Anthony Cramp <anthony.cramp@gmail.com>
2021-06-28 18:54:22 +0930

    Day 14 Part 1 solution

--- day14/notes.md
+++ day14/notes.md
@@ -3,3 +3,16 @@
 [Advent of Code 2015 Day 14](https://adventofcode.com/2015/day/14)
 
 ## Thoughts
+
+- Simple time stepped simulation?
+- Could be an event driven simulation to improve performance
+- Suspect Part 2 will introduce some inter time checks that will benefit from a
+  time stepped, rather then event stepped, approach
+- Going to implement this myself for now but I'm sure there are Rust sim engines
+  that could be leveraged
+- Actually, Part 1 can be solved analytically. For a reindeer that can do speed
+  S for t_s seconds and then rests for t_r seconds. This combination is the
+  cycle time t_c = t_s + t_r. Over t seconds, it goes through A = t / t_c
+  complete cycles and one partial cycle of duration B = t % t_c. Total distance
+  covered is s \* A + s \* min(t_s, B). (although, I still think Part 2 will
+  require an iterative solution)
--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,3 +1,8 @@
+use regex::Regex;
+
+#[macro_use]
+extern crate lazy_static;
+
 const INPUT: &str = include_str!("day14.txt");
 
 fn main() {
@@ -5,14 +10,76 @@ fn main() {
     println!("Day 14 Part 2: {:?}", part2(INPUT));
 }
 
-// replace return type as required by the problem
-fn part1(input: &str) -> i32 {
+fn part1(input: &str) -> u32 {
+    Race::from(input).get_winning_distance(2503)
+}
+
+fn part2(input: &str) -> i32 {
     0
 }
 
-// replace return type as required by the problem
-fn part2(input: &str) -> i32 {
-    0
+#[derive(Debug, Clone, Eq, PartialEq)]
+struct Reindeer {
+    name: String,
+    speed: u32,
+    speed_duration: u32,
+    rest_duration: u32,
+}
+
+impl From<&str> for Reindeer {
+    fn from(input: &str) -> Self {
+        lazy_static! {
+            static ref RE: Regex = Regex::new(
+                r"(.*) can fly (.*) km/s for (.*) seconds, but then must rest for (.*) seconds."
+            )
+            .unwrap();
+        }
+
+        let fields = RE.captures(input).unwrap();
+
+        Self {
+            name: String::from(fields.get(1).unwrap().as_str()),
+            speed: fields.get(2).unwrap().as_str().parse().unwrap(),
+            speed_duration: fields.get(3).unwrap().as_str().parse().unwrap(),
+            rest_duration: fields.get(4).unwrap().as_str().parse().unwrap(),
+        }
+    }
+}
+
+impl Reindeer {
+    fn calculate_distance_travelled(&self, t: u32) -> u32 {
+        let cycle_duration = self.speed_duration + self.rest_duration;
+        let num_cycles = t / cycle_duration;
+        let partial_end_cycle_duration = t % cycle_duration;
+        let time_at_speed = num_cycles * self.speed_duration
+            + std::cmp::min(self.speed_duration, partial_end_cycle_duration);
+        self.speed * time_at_speed
+    }
+}
+
+struct Race {
+    reindeer: Vec<Reindeer>,
+}
+
+impl From<&str> for Race {
+    fn from(input: &str) -> Self {
+        let mut reindeer = vec![];
+        for line in input.lines() {
+            reindeer.push(Reindeer::from(line));
+        }
+
+        Self { reindeer }
+    }
+}
+
+impl Race {
+    fn get_winning_distance(&self, t: u32) -> u32 {
+        self.reindeer
+            .iter()
+            .map(|r| r.calculate_distance_travelled(t))
+            .max()
+            .unwrap()
+    }
 }
 
 #[cfg(test)]
@@ -21,36 +88,52 @@ mod tests {
     use test_support::test_support::TestCase;
 
     #[test]
-    fn test_part1() {
-        let test_cases = [
-            TestCase {
-                input: "...",
-                expected: 123,
-            },
-            TestCase {
-                input: "abc",
-                expected: 345,
-            },
-        ];
-        for TestCase { input, expected } in test_cases.iter() {
-            assert_eq!(part1(*input), *expected);
-        }
+    fn test_reindeer_parse() {
+        let input = "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.";
+        let comet = Reindeer::from(input);
+
+        assert_eq!(
+            comet,
+            Reindeer {
+                name: String::from("Comet"),
+                speed: 14,
+                speed_duration: 10,
+                rest_duration: 127
+            }
+        );
+
+        let input = "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.";
+        let dancer = Reindeer::from(input);
+
+        assert_eq!(
+            dancer,
+            Reindeer {
+                name: String::from("Dancer"),
+                speed: 16,
+                speed_duration: 11,
+                rest_duration: 162
+            }
+        );
     }
 
     #[test]
-    fn test_part2() {
-        let test_cases = [
-            TestCase {
-                input: "...",
-                expected: 123,
-            },
-            TestCase {
-                input: "abc",
-                expected: 345,
-            },
-        ];
-        for TestCase { input, expected } in test_cases.iter() {
-            assert_eq!(part2(*input), *expected);
-        }
+    fn test_distance_travelled() {
+        let input = "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.";
+        let comet = Reindeer::from(input);
+
+        assert_eq!(comet.calculate_distance_travelled(1000), 1120);
+
+        let input = "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.";
+        let dancer = Reindeer::from(input);
+
+        assert_eq!(dancer.calculate_distance_travelled(1000), 1056);
+    }
+
+    #[test]
+    fn test_get_winning_distance() {
+        let input = r"Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.";
+        let race = Race::from(input);
+        assert_eq!(race.get_winning_distance(1000), 1120);
     }
 }
