1a960ed0bf5381f291e49ea9a0ea7d1b527ac8b9
Anthony Cramp <anthony.cramp@gmail.com>
2021-06-30 22:49:06 +0930

    Added Day 16 skeleton

---
+++ day16/notes.md
@@ -0,0 +1,8 @@
+# Advent of Code 2015 Day 16
+
+[Advent of Code 2015 Day 16](https://adventofcode.com/2015/day/16)
+
+## Thoughts
+
+- seems like a fairly simple parse and match problem
+- Option to the rescue!
---
+++ day16/src/main.rs
@@ -0,0 +1,29 @@
+use regex::Regex;
+
+#[macro_use]
+extern crate lazy_static;
+
+const INPUT: &str = include_str!("day16.txt");
+
+fn main() {
+    println!("Day 16 Part 1: {:?}", part1(INPUT));
+    println!("Day 16 Part 2: {:?}", part2(INPUT));
+}
+
+// replace return type as required by the problem
+fn part1(input: &str) -> i32 {
+    0
+}
+
+// replace return type as required by the problem
+fn part2(input: &str) -> i32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse() {}
+}
