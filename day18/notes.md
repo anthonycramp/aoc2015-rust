67409f3d19d49b89ac781d8c780c3b0b08fbb5ea
Anthony Cramp <anthony.cramp@gmail.com>
2021-07-02 18:42:12 +0930

    Added Day 18 skeleton

---
+++ day18/notes.md
@@ -0,0 +1,9 @@
+# Advent of Code 2015 Day 18
+
+[Advent of Code 2015 Day 18](https://adventofcode.com/2015/day/18)
+
+## Thoughts
+
+- Literally Conway's Game of Life
+- Store things in arrays that are two bigger so that the edge is captured.
+  Iterate over 1..=100 in an array with indices from 0 to 101.
---
+++ day18/src/main.rs
@@ -0,0 +1,27 @@
+const INPUT: &str = include_str!("day18.txt");
+
+fn main() {
+    println!("Day 18 Part 1: {:?}", part1(INPUT));
+    println!("Day 18 Part 2: {:?}", part2(INPUT));
+}
+
+// replace return type as required by the problem
+fn part1(input: &str) -> i32 {
+    0
+}
+
+// replace return type as required by the problem
+fn part2(input: &str) -> i32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {}
+
+    #[test]
+    fn test_part2() {}
+}
